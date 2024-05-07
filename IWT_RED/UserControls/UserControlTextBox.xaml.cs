ccdf1fa34862dfd8269d52d4829ba80f6f828439
KiFiRCHeK401 <artur-karel@yandex.ru>
2024-05-07 14:12:05 +0400

    Update5
    Using UserControl

---
+++ IWT_RED/UserControls/UserControlTextBox.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace IWT_RED
+{
+    /// <summary>
+    /// Логика взаимодействия для UserControlTextBox.xaml
+    /// </summary>
+    public partial class UserControlTextBox : UserControl
+    {
+        public string TextForPlaceHolder { get; set; }
+
+        public UserControlTextBox()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+    }
+}
